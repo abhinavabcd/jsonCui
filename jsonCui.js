3f5cd94bd16ea530e28b78fc9b0e427d2a9600d7
abhinav2 <asolipuram@kiwiup.com>
2014-05-28 17:24:42 +0530

    minor changes to add hooks

--- jsonCui.js
+++ jsonCui.js
@@ -22,7 +22,7 @@ function jsonCui(obj,headersObj,div,$){
     	if($.isArray(keySet)){
 	    	c=0;
 	    	while(c < keySet.length-1 ){
-	    		if(!obj.hasOwnProperty([keySet[c]]))
+	    		if(!obj.hasOwnProperty(keySet[c]))
 	    			obj[[keySet[c]]] = {}
 	    		
 	    		obj = obj[keySet[c]];
@@ -79,10 +79,16 @@ function jsonCui(obj,headersObj,div,$){
     	return this.get(keySet,this.hooks);
     };
     
-    this.createCui = function(){
+    this.createCui = function(objClass,listClass){
+
+    	this.objClass= $.isArray(objClass)?objClass:[];
+    	this.listClass = $.isArray(listClass)?listClass:[];
     	this.addObjCui(this.div, []);
     };
-    this.addObjCui = function(elem , keySet){
+    this.addObjCui = function(elem , keySet ){
+    	var objClass = this.objClass;
+    	var listClass = this.listClass;
+    	var tempThis = this;
 		var parentObj = this.obj; 
 		var obj = this.get(keySet);
 		var hook = this.getHook(keySet);
@@ -93,8 +99,11 @@ function jsonCui(obj,headersObj,div,$){
 			
 		if($.isPlainObject(obj)){
 
-			var table = $('<table><tbody></tbody></table>');
-			table[0].parentObj = parentObj
+			var table = $('<table><thead><tr><th>Name</th><th>Property</th></tr></thead><tbody></tbody></table>');
+			for(var i=0;i<objClass.length;i++){
+				table.addClass(objClass[i]);
+			}
+			table[0].parentObj = parentObj;
 			table[0].keySet = keySet;
 			var _table = table[0];
 			var div= $("<div>").addClass("objContain");
@@ -102,7 +111,7 @@ function jsonCui(obj,headersObj,div,$){
 					$("<div>")
 						.addClass("del")
 						.click(function(){ deleteElement(div); parentObj[key]=obj; this.addObjCui(elem,keySet);  })
-					);//.append(
+				);//.append(
 				//	$("<div>").addClass("addNewRow").click(function(){addTableRow(table,"stringInput","stringInput");})
 				//);
 			elem.append(div);	
@@ -117,9 +126,11 @@ function jsonCui(obj,headersObj,div,$){
 		if($.type( obj )==="array"){
 			var v = obj.length ? obj[0] : "string";
 			var table = $('<table><tbody></tbody></table>');
+			for(var i=0;i<listClass.length;i++){
+				table.addClass(listClass[i]);
+			}
 			table[0].parentObj = parentObj;
 			table[0].keySet = keySet;
-			var tempThis = this;
 			var addRow = function (v,keySet2){
 				return function(){
 				 var tr = $('<tr>').appendTo(table);
@@ -148,7 +159,7 @@ function jsonCui(obj,headersObj,div,$){
 				$("<div>").appendTo(elem).append(obj).click(function(e){
 					var p = prompt("Please enter text", obj);
 					$(this).html(p ? p : 'null');
-					this.set(keySet, p);
+					tempThis.set(keySet, p);
 				});
 		}
 		
@@ -159,15 +170,16 @@ function jsonCui(obj,headersObj,div,$){
 				set(keySet, p);
 			});
 		}
-		if($.type( obj ) === "boolean"){			
+		if($.type( obj ) === "boolean"){		
+			
 			$("<input type='checkbox'>").appendTo(elem).click(function(e){
-				    var $this = $(this);
-				    if ($this.is(':checked')) {
-				    	set(keySet, true);
+				    var check = $(this);
+				    if (check.is(':checked')) {
+				    	tempThis.set(keySet, true);
 				    } else {
-				    	set(keySet,false);
+				    	tempThis.set(keySet,false);
 				    }
-				});
+				}).prop('checked', obj);
 		}			
 	}
 }
